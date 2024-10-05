3fcf61a8373ebda9e40fd407a16be92a3da55dda
Razvan Ciopraga <razvan.ciopraga@ru.nl>
2024-10-06 00:54:21 +0200

    Created interface class for model operations

---
+++ MangaOCR/OCRModel/ModelAPIs/ModelInterfaces/ModelInterface.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from numpy.typing import NDArray
+from OCRModel.ModelAPIs.Utils.DataRepresentation import TextPrediction
+from typing import Iterable
+class ModelInterface(ABC):
+    def get_model_info(self) -> str:
+        raise NotImplementedError("Interface method was not implemented!")
+    def process_image(self, image:NDArray[float]) -> Iterable[TextPrediction]:
+        raise NotImplementedError("Interface method was not implemented!")
+    def process_images(self, images:list[NDArray[float]]) -> Iterable[Iterable[TextPrediction]]:
+        raise NotImplementedError("Interface method was not implemented!")
